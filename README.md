74471989aff4d827e3ee71a12f80c7525cec8ae6
Mike Straw <mike.straw@automattic.com>
2024-01-22 10:56:03 -0500

    Update title and README. Build

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # blocks-playground
  My first blocks. This is just a place for me to play with Gutenburg blocks to learn more about them.
+
+- Ran through the Quick Start Guide doc and video, creating the [Copyright Date Block](copyright-date-block).
--- copyright-date-block/build/index.asset.php
+++ copyright-date-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '18c3ca7e896f1554b990');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '61e3d707afba8896c114');
